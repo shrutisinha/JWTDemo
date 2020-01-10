f5c7748fc9159ee5f1082bb2cfacdf1d12efaa02
Shruti <shruti.sinha@zemosolabs.com>
2020-01-10 15:12:21 +0530

    JWT demo

---
+++ src/main/java/com/samplejwt/service/JwtUserDetailsService.java
@@ -0,0 +1,24 @@
+package com.samplejwt.service;
+
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
+public class JwtUserDetailsService implements UserDetailsService {
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        if("samplejwt".equals(username)) {
+            String password = new BCryptPasswordEncoder().encode("password");
+            return new User("samplejwt", password,
+                    new ArrayList<>());
+        } else {
+            throw new UsernameNotFoundException("User not found with username: " + username);
+        }
+    }
+}
